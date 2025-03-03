2ff5b5fc550c35cc9cc694be87e5ec5d9bb77012
Guillaume Even <daguinci@mailo.com>
2025-03-03 14:42:09 +0100

    initialize Django project structure with basic settings, URLs, and ASGI/WSGI configurations

---
+++ failalka/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for failalka project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'failalka.settings')
+
+application = get_wsgi_application()
