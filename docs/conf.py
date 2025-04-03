3ef4fd4b3f5e118bc8286d03ac6463c83ffb1521
Guillaume Even <daguinci@mailo.com>
2025-04-03 12:04:58 +0200

    Add user manual and update documentation requirements; switch to dark mode theme

--- docs/conf.py
+++ docs/conf.py
@@ -17,6 +17,7 @@ author = 'Guillaume Even'
 extensions = [
     'sphinx.ext.autosectionlabel',
     'sphinxcontrib.plantuml',
+    'sphinx_rtd_dark_mode'
 ]
 
 templates_path = ['_templates']
@@ -28,10 +29,15 @@ master_doc = 'index'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_dark_mode'
 html_static_path = ['_static']
 html_css_files = [
     'css/erd.css',
 ]
 
 html_show_sourcelink = False
+# user starts in dark mode
+default_dark_mode = True
+
+# user starts in light mode
+# default_dark_mode = False
--- docs/index.rst
+++ docs/index.rst
@@ -3,12 +3,12 @@
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome on the website Failalka's documentation
+Welcome on the Failalka's website documentation
 =================================================
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
 
+   source/use
    source/development
-   source/usage
