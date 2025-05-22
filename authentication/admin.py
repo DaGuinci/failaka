264573f17628e3188a9223dc970333cfd9a1933d
Guillaume Even <daguinci@mailo.com>
2025-05-22 16:53:09 +0200

    Refactor user management: implement custom user creation form, update UserAdmin to exclude username and user permissions, and enforce single group assignment in save method.

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as DefaultUserAdmin
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import Group
 from .models import User
 
@@ -9,6 +10,7 @@ class UserAdminForm(forms.ModelForm):
     class Meta:
         model = User
         fields = '__all__'
+        exclude = ['user_permissions', 'username']  # Exclure les permissions utilisateur et le champ username
 
     def clean_groups(self):
         groups = self.cleaned_data.get('groups')
@@ -17,13 +19,37 @@ class UserAdminForm(forms.ModelForm):
         return groups
 
 
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'last_name')  # Champs affichés dans le formulaire
+
+
 @admin.register(User)
 class UserAdmin(DefaultUserAdmin):
     form = UserAdminForm
+    add_form = CustomUserCreationForm  # Utilise le formulaire personnalisé pour l'ajout
     list_display = ('email', 'first_name', 'last_name', 'is_staff', 'get_group')
     list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
     search_fields = ('email', 'first_name', 'last_name')
     ordering = ('email',)
+
+    # Exclure les permissions utilisateur des formulaires
+    fieldsets = (
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Groups', {'fields': ('groups',)}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'first_name', 'last_name', 'password1', 'password2'),
+        }),
+    )
+
+    readonly_fields = ('password',)  # Rend le champ 'password' en lecture seule
 
     def get_group(self, obj):
         return ", ".join([group.name for group in obj.groups.all()])
--- authentication/models.py
+++ authentication/models.py
@@ -4,30 +4,22 @@ from django.contrib.auth.models import AbstractUser, Group
 from django.db import models
 
 class User(AbstractUser):
+    username = None  # Supprime le champ username
+    email = models.EmailField(unique=True)  # Utilise email comme identifiant unique
+
+    USERNAME_FIELD = 'email'  # Définit email comme champ principal
+    REQUIRED_FIELDS = []  # Aucun champ supplémentaire requis
+
+    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+    thumbnail = models.ImageField(upload_to='users/', blank=True, null=True)
+
     def save(self, *args, **kwargs):
         super().save(*args, **kwargs)
         if self.groups.count() > 1:
             # Si l'utilisateur appartient à plusieurs groupes, ne garder que le dernier ajouté
             last_group = self.groups.last()
             self.groups.set([last_group])
-
-#   - UUID: UUID
-#   - firstname: String
-#   - lastname: String
-#   - groupe: Group
-#   - email: String
-#   - password: String
-#   - thumbnail: path
-
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = (["password"])
-
-
-    id = models.UUIDField(primary_key=True, editable=False, default=uuid.uuid4)
-    username = models.CharField(max_length=150, blank=True)
-    first_name = models.CharField(max_length=150, blank=True)
-    last_name = models.CharField(max_length=150, blank=True)
-    email = models.EmailField(max_length=150, unique=True, blank=False, null=False)
-    thumbnail = models.ImageField(upload_to='users/', blank=True, null=True)
 
 
