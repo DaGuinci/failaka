098a3ebaeb0f6cc9e93e37f425dfaa01d7fe5b5a
Guillaume Even <daguinci@mailo.com>
2025-05-22 11:08:45 +0200

    Remove unused database initialization commands and related user/entity creation scripts

--- authentication/apps.py
+++ authentication/apps.py
@@ -7,11 +7,3 @@ class AuthenticationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'authentication'
 
-    # Drop and recreate the database:
-    def ready(self):
-        if os.environ.get('DJANGO_ENV') == 'development':
-            # from django.contrib.auth.models import User
-            # call_command('flush', '--no-input')
-            # call_command('migrate')
-            # call_command('create_users')
-            pass
