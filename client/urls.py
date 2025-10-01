67fd1958953b9313cc4c881bb7a2e8f65caf4ad3
Guillaume Even <daguinci@mailo.com>
2025-10-01 12:03:40 +0200

    Add sites list view and template for displaying sites

--- client/urls.py
+++ client/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.items_list, name='home'),
     path('items/<uuid:item_uuid>/', views.item_detail, name='item_detail'),
+    path('sites/', views.sites_list, name='sites_list'),
     path('sites/<uuid:site_uuid>/', views.site_detail, name='site_detail'),
 ]
