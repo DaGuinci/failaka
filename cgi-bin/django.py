13b09737cec3909797903f3fc6d718a1748579ac
Guillaume Even <daguinci@mailo.com>
2025-10-10 00:49:40 +0200

    Fix Django CGI script path to be dynamic instead of hardcoded

--- cgi-bin/django.py
+++ cgi-bin/django.py
@@ -7,7 +7,9 @@ import django
 from django.core.wsgi import get_wsgi_application
 
 # Ajouter le répertoire de l'application au Python path
-sys.path.insert(0, '/home/gufo9311/www')
+import os
+script_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, script_dir)
 
 # Définir le module de configuration Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'failaka.settings')
